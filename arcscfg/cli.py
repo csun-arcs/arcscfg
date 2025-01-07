b551ea32dfc36081bf08a262018e8abcef481b48
Barry Ridge <barry.ridge@csun.edu>
2025-01-07 15:03:18 -0800

    Add workspace path finding logic

--- arcscfg/cli.py
+++ arcscfg/cli.py
@@ -1,15 +1,26 @@
 # arcscfg/arcscfg/cli.py
 
+import os
 import argparse
+from pathlib import Path
+from arcscfg.utils.setup_workspace import setup_workspace
+
+def get_available_workspaces():
+    workspaces_dir = Path(os.path.join(Path(__file__).parent, "config/workspaces"))
+    return [f.name for f in workspaces_dir.glob("*.yaml")]
 
 def main():
     parser = argparse.ArgumentParser(description="ARCS environment configurator")
     parser.add_argument("command", choices=["setup", "install", "build"], help="Command to execute")
-    parser.add_argument("--workspace", help="Path to the workspace configuration file")
+    parser.add_argument("--workspace", help="Path to the workspace configuration file", choices=get_available_workspaces())
     args = parser.parse_args()
 
     if args.command == "setup":
-        print(f"Setting up workspace using {args.workspace}")
+        if not args.workspace:
+            print("Error: --workspace argument is required for the 'setup' command.")
+            return
+        workspace_config = Path(os.path.join(Path(__file__).parent, "config/workspaces", args.workspace))
+        setup_workspace(workspace_config)
     elif args.command == "install":
         print("Installing dependencies")
     elif args.command == "build":
