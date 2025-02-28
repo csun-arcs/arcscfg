d292993c87bb54fc65d7aa1ba88384c4187aed44
Barry Ridge <barry.ridge@csun.edu>
2025-02-28 08:38:21 -0800

    Add missing required packages to setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,12 @@ setup(
     packages=find_packages(include=["arcscfg", "arcscfg.*"]),
     include_package_data=True,
     install_requires=[
+        "setuptools", # For installation
         "pyyaml", # For YAML parsing
-        "colorlog",  # For colored logging
+        "pytest", # For testing
+        "colorlog", # For colored logging
         "appdirs", # For finding log dirs on different systems
+        "colcon-common-extensions" # For ROS 2 builds
     ],
     entry_points={
         "console_scripts": [
