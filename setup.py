9ccf6eb037a679dfe2cd0dca44bcd3c3c0bef407
Barry Ridge <barry.ridge@csun.edu>
2025-02-28 09:03:14 -0800

    Move pytest from install_requires - extras_require

--- setup.py
+++ setup.py
@@ -9,11 +9,13 @@ setup(
     include_package_data=True,
     install_requires=[
         "pyyaml", # For YAML parsing
-        "pytest", # For testing
         "colorlog", # For colored logging
         "appdirs", # For finding log dirs on different systems
         "colcon-common-extensions" # For ROS 2 builds
     ],
+    extras_require={
+        "dev": ["pytest"] # For testing
+    },
     entry_points={
         "console_scripts": [
             "arcscfg = arcscfg.cli:main",
